41eba1a0e8da0b8985ef19b7d0ff637b155fd7c9
abubakar979 <programmer.abubakar97@gmail.com>
2026-01-06 09:14:07 +0600

    Cf Div 2

---
+++ A_Scarborough_Fair.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+ int n,m;
+ string s;
+ cin>>n>>m;
+ cin>>s;
+ for(int i=0;i< m;i++){
+  int l,r;
+  char c1,c2;
+  cin>>l>>r>>c1>>c2;
+  for(int j=l-1;j<r;j++){
+    if(s[j]==c1){
+       s[j]=c2;
+    }
+  }
+
+ }
+ cout<<s;
+
+
+return 0;
+}
