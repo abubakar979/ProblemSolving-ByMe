4424c89a3978780b8e94e1df56930e45ae0a41f4
abubakar979 <programmer.abubakar97@gmail.com>
2026-01-06 10:32:14 +0600

    Cf_Div_2_A

---
+++ A_Tokitsukaze and Enhancement.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n % 4 == 1){
+        cout << 0 << " A";
+        return 0;
+    }
+    else if(n % 4 == 0){
+        cout << 1 << " A";
+        return 0;
+    }
+    else if(n % 4 == 3){
+        cout << 2 << " A";
+        return 0;
+    }
+    else{
+        cout << 1 << " B";
+        return 0;
+    }
+}
