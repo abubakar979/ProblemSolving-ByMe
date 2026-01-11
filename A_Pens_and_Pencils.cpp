1cf5d42da55c8da1cac19ad1faf6699af992665e
abubakar979 <programmer.abubakar97@gmail.com>
2026-01-11 12:44:40 +0600

    Codeforces Div 2 A

---
+++ A_Pens_and_Pencils.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c,d,k;
+        cin >> a >> b >> c >> d >> k;
+
+        int pens = (a + c - 1) / c;
+        int pencils = (b + d - 1) / d;
+
+        if(pens + pencils > k){
+            cout << -1 << endl;
+        } else {
+            cout << pens << " " << pencils << endl;
+        }
+    }
+    return 0;
+}
