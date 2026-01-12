efa34d01b18ddb91b5b78f14fe4e8796e3596b8d
abubakar979 <programmer.abubakar97@gmail.com>
2026-01-12 13:49:55 +0600

    CF_DIV__2__A

---
+++ A. Ehab and another construction problem.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+
+    if(x % 2 == 0){
+        cout << x << " " << 2 << endl;
+    } else {
+        cout << -1 << endl;
+    }
+
+    return 0;
+}
+
